7919d27e26ddb54067b447338faa35ec2a4d2e32
devkwonsehoon <devkwonsehoon@naver.com>
2021-12-02 12:58:03 +0900

    update: 상태 코드 통일

--- functions/api/routes/radio/radioGET.js
+++ functions/api/routes/radio/radioGET.js
@@ -1,13 +1,13 @@
 const functions = require('firebase-functions');
-const util = require('../../../lib/util');
-const statusCode = require('../../../constants/statusCode');
-const responseMessage = require('../../../constants/responseMessage');
+const { success, fail } = require('../../../lib/util');
+const sc = require('../../../constants/statusCode');
+const rm = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
 const { hugDB } = require('../../../db');
 
 module.exports = async (req, res) => {
   const { hugId } = req.params;
-  if (!hugId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  if (!hugId) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NULL_VALUE));
 
   let client;
 
@@ -15,12 +15,12 @@ module.exports = async (req, res) => {
     client = await db.connect(req);
 
     const hug = await hugDB.getHugById(client, hugId);
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.GET_MUSICHUG_INFO_SUCCESS, hug));
+    res.status(sc.OK).send(success(sc.OK, rm.GET_MUSICHUG_INFO_SUCCESS, hug));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
 
-    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+    res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
   }
--- functions/api/routes/radio/radioPOST.js
+++ functions/api/routes/radio/radioPOST.js
@@ -2,16 +2,16 @@
 // by HYOSITIVE
 
 const functions = require('firebase-functions');
-const util = require('../../../lib/util');
-const statusCode = require('../../../constants/statusCode');
-const responseMessage = require('../../../constants/responseMessage');
+const { success, fail } = require('../../../lib/util');
+const sc = require('../../../constants/statusCode');
+const rm = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
 const { hugDB } = require('../../../db');
 
 module.exports = async (req, res) => {
   const { hugTitle, nickname } = req.body;
 
-  if (!hugTitle || !nickname) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ADD_MUSICHUG_FAIL));
+  if (!hugTitle || !nickname) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.ADD_MUSICHUG_FAIL));
 
   let client;
 
@@ -20,12 +20,12 @@ module.exports = async (req, res) => {
 
     const newHug = await hugDB.createHug(client, hugTitle, nickname);
 
-    res.status(statusCode.OK).send(util.success(statusCode.CREATED, responseMessage.ADD_MUSICHUG_SUCCESS, newHug));
+    res.status(sc.CREATED).send(success(sc.CREATED, rm.ADD_MUSICHUG_SUCCESS, newHug));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
 
-    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+    res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
   }
